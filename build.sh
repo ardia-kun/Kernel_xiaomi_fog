3ca232e2858c5e02a6c4e5f88822b0dd1ac6bbb8
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2024-11-18 05:06:21 +0000

    Build: rewrite all command for build
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,78 +1,34 @@
-#!/bin/bash
-#
-# Compile script for uvite Kernel
-# Copyright (C) 2020-2021 Adithya R.
+export TZ='Asia/Jakarta'
+BUILDDATE=$(date +%Y%m%d)
+NAME=Swordx-Juzumaru
+# BUILDTIME=$(date +%H%M)
 
-SECONDS=0 # builtin bash timer
-ZIPNAME="uvite-$(date '+%Y%m%d-%H%M')-spes.zip"
-TC_DIR="$(pwd)/tc/clang-r450784e"
-AK3_DIR="$(pwd)/android/AnyKernel3"
-DEFCONFIG="vendor/spes-perf_defconfig"
+# Install dependencies
+sudo apt update && sudo apt install -y bc cpio nano bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu
 
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
-fi
+# Using Rastamod-Clang
+wget https://github.com/kutemeikito/RastaMod69-Clang/releases/download/RastaMod69-Clang-20.0.0-release/RastaMod69-Clang-20.0.0.tar.gz
+# Move & Extract Clang
+mkdir clang && tar -xf RastaMod69-Clang-20.0.0.tar.gz -C clang && rm -rf RastaMod69-Clang-20.0.0.tar.gz
 
-export PATH="$TC_DIR/bin:$PATH"
-export KBUILD_BUILD_USER=nobody
-export KBUILD_BUILD_HOST=android-build
+# Set variable
+export KBUILD_BUILD_USER=kiddie
+export KBUILD_BUILD_HOST=ubuntu
 
-if ! [ -d "$TC_DIR" ]; then
-	echo "AOSP clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
-fi
+    # Build
+    # Prepare
+    make -j$(nproc --all) O=out ARCH=arm64 CC=$(pwd)/clang/bin/clang CROSS_COMPILE=aarch64-linux-gnu- CLANG_TRIPLE=aarch64-linux-gnu- LLVM_IAS=1 vendor/fog-perf_defconfig
+    # Execute
+    make -j$(nproc --all) O=out ARCH=arm64 CC=$(pwd)/clang/bin/clang CROSS_COMPILE=aarch64-linux-gnu- CLANG_TRIPLE=aarch64-linux-gnu- LLVM_IAS=1
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-	rm -rf out
-fi
-
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
-
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
-
-kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dtb.img"
-dtbo="out/arch/arm64/boot/dtbo.img"
-
-if [ -f "$kernel" ]; then
-	echo -e "\nKernel compiled succesfully! Zipping up...\n"
-	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
-		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-		exit 1
-	fi
-	cp $kernel $dtb $dtbo AnyKernel3
-	rm -rf out/arch/arm64/boot
-	cd AnyKernel3
-	git checkout master &> /dev/null
-	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
-	cd ..
-	rm -rf AnyKernel3
-	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-	echo "Zip: $ZIPNAME"
-else
-	echo -e "\nCompilation failed!"
-	exit 1
-fi
-
+    # Package
+    git clone --depth=1 https://github.com/ardia-kun/AnyKernel3-680 -b ksu AnyKernel3
+    cp -R out/arch/arm64/boot/Image.gz AnyKernel3/Image.gz
+    # Zip it and upload it
+    cd AnyKernel3
+    zip -r9 $NAME+KSU-"$BUILDDATE" . -x ".git*" -x "README.md" -x "*.zip"
+    curl -T $NAME+KSU-"$BUILDDATE".zip -u :d8fce976-eb03-4c70-be68-aaeb1fe8fced https://pixeldrain.com/api/file/
+    # finish
+    cd ..
+    rm -rf clang-llvm/ AnyKernel3/
+    echo "Build finished"
